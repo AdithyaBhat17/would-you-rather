8e656024b115e812ad5e449111fdb70da8d6afed
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-09-18 15:58:35 +0530

    Feat: Used browser router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/index.css';
 import App from './components/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+import middleware from './middleware';
+import { BrowserRouter } from 'react-router-dom';
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
