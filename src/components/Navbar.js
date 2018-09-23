c6f9f856cf656cb54dd47b9db0f4abf836c7b161
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2018-09-23 17:53:06 +0530

    Refactor : Function components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,37 +2,37 @@ import React from 'react';
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
-    <nav className="navbar navbar-default">
-        <div className="container">
-            <div className="navbar-header">
-                <button 
-                type="button" 
-                className="navbar-toggle collapsed" 
-                data-toggle="collapse" 
-                data-target="#bs-example-navbar-collapse-1" 
-                aria-expanded="false">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <Link
-                to="/"
-                className="navbar-brand"
-                >
-                    Would you rather
-                </Link>
+export default function Navbar(){
+    return(
+        <nav className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                    <button 
+                    type="button" 
+                    className="navbar-toggle collapsed" 
+                    data-toggle="collapse" 
+                    data-target="#bs-example-navbar-collapse-1" 
+                    aria-expanded="false">
+                        <span className="sr-only">Toggle navigation</span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </button>
+                    <Link
+                    to="/"
+                    className="navbar-brand"
+                    >
+                        Would you rather
+                    </Link>
+                </div>
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul className="nav navbar-nav navbar-right">
+                        <li><Link to="/login">LOGIN</Link></li>
+                    </ul>
+                </div>
             </div>
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav navbar-right">
-                    <li><Link to="/login">LOGIN</Link></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-);
+        </nav>
+    );
+}
 
-
-export default Navbar;
 
